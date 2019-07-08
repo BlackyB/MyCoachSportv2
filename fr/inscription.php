afd2c1ccd8c06319b98a420165ac447d5d51a5d6
Bari Jonathan <bari.jonathan@live.fr>
2019-07-08 10:12:49 +0200

    Test Footer Newsletter

--- fr/inscription.php
+++ fr/inscription.php
@@ -1,19 +1,19 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <?php include('./head.php'); ?>
+  <?php include('/fr/head.php'); ?>
 
   <title>My Coach Sport - Inscription Validée</title>
 </head>
 <body class="col full-H justify-between">
-  <?php include('./header.php'); ?>
+  <?php include('/fr/header.php'); ?>
 
   <div class="col justify-center align-center w-75 m-auto">       
     <h2 class="txt-center">Votre inscription à la newsletter My Coach Sport est enregistrée.</h2>
     <a class="m-Top-60" href="/fr/index.php"><div class=" col justify-center button-Connexion"><p class="section-Texte txt-center">Retour à l'Accueil</div></a>
     </div>
     
-    <?php include('./footer.php'); ?>
+    <?php include('/fr/footer.php'); ?>
 
   </body>
   </html>
