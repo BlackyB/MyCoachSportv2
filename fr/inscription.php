7ac98d64161556366ddff6fa6427ae849db217f8
Bari Jonathan <bari.jonathan@live.fr>
2019-07-08 10:11:10 +0200

    Test Footer Newsletter

--- fr/inscription.php
+++ fr/inscription.php
@@ -1,32 +1,19 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <?php include('head.inc.php'); ?>
+  <?php include('./head.php'); ?>
 
-  <title>My Coach Vélo by FFC</title>
+  <title>My Coach Sport - Inscription Validée</title>
 </head>
-<body id="apropos">
-  <header class="container-fluid">
-    <div class="container p-0">
-        <?php $page = 'apropos'; include('menu.inc.php'); ?>
+<body class="col full-H justify-between">
+  <?php include('./header.php'); ?>
+
+  <div class="col justify-center align-center w-75 m-auto">       
+    <h2 class="txt-center">Votre inscription à la newsletter My Coach Sport est enregistrée.</h2>
+    <a class="m-Top-60" href="/fr/index.php"><div class=" col justify-center button-Connexion"><p class="section-Texte txt-center">Retour à l'Accueil</div></a>
     </div>
     
-  </header>
-  <main class="container">
-    <div class="row"> 
-      <div class="col col-lg-8 offset-lg-2">       
-        <h2 class="my-5 py-3 text-center">Votre inscription à la newsletter My Coach Vélo est enregistrée.</h2>
-      </div>
-    </div>
-  </main>
-  
+    <?php include('./footer.php'); ?>
 
-<?php include('footer.inc.php'); ?>
-<?php include('foot.inc.php'); ?>
-<script>
-$('html,body').animate({
-  scrollTop: $(window.location.hash).offset().top
-});
-</script>
-</body>
-</html>
+  </body>
+  </html>
