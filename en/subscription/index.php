70c20fe0f32d48053e79d23c8f8494fb9d42e51b
Bari Jonathan <bari.jonathan@live.fr>
2019-07-09 11:11:51 +0200

    zdz

---
+++ en/subscription/index.php
@@ -0,0 +1,23 @@
+    <!doctype html>
+    <html lang="fr">
+    <head>
+      <?php include('../head.inc.php'); ?>
+
+      <title>My Coach Sport - Subscription Confirmed</title>
+    </head>
+    <body class="full-H d-flex flex-column justify-content-between">
+      <div class="container p-0">
+        <?php include('../menu.inc.php'); ?>
+      </div>
+
+      <div class="d-flex flex-column align-items-center">       
+        <h2 class="txt-center">Subsciption to My Coach Sport's newsletter validated.</h2>
+        <a class="m-Top-60" href="../index.php"><div class=" col justify-center button-Connexion"><p class="section-Texte txt-center">Back to homepage</div></a>
+        </div>
+        <div class="container-fluid p-0">
+          <?php include('footer.inc.php'); ?>
+          <?php include('../foot.inc.php'); ?>
+        </div>
+
+      </body>
+      </html>
--- es/suscripcion/index.php
+++ es/suscripcion/index.php
@@ -1,28 +1,23 @@
-<!doctype html>
-<html lang="es">
-<head>
-  <?php include('../head.inc.php'); ?>
-  <meta name="description" content="Start-up française créée en 2011 et spécialisée dans la conception de solutions numériques dans le secteur du sport." />
-  <meta property="og:url" content="https://www.mycoachsport.com/es/" />
-  <meta property="og:title" content="My Coach Sport - Créateur de solutions numériques" />
-  <meta property="og:description" content="Start-up française créée en 2011 et spécialisée dans la conception de solutions numériques dans le secteur du sport." />
-  <meta name="twitter:description" content="Start-up française créée en 2011 et spécialisée dans la conception de solutions numériques dans le secteur du sport." />
-  <link rel="alternate" hreflang="en" href="https://www.mycoachsport.com/en/" />
-  <link rel="alternate" hreflang="fr" href="https://www.mycoachsport.com/fr/" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+  <!doctype html>
+  <html lang="fr">
+  <head>
+    <?php include('../head.inc.php'); ?>
 
-  <title>My Coach Sport - Registro válido</title>
-</head>
-<body class="col full-H justify-between">
-  <?php include('../menu.inc.php'); ?>
-
-  <div class="col justify-center align-center w-75 m-auto">       
-    <h2 class="txt-center">La suscriptión al boletín de My Coach Sport está validada.</h2>
-    <a class="m-Top-60" href="../index.php"><div class=" col justify-center button-Connexion"><p class="section-Texte txt-center">Volver a la página de inicio</div></a>
+    <title>My Coach Sport - Registro válido</title>
+  </head>
+  <body class="full-H d-flex flex-column justify-content-between">
+    <div class="container p-0">
+      <?php include('../menu.inc.php'); ?>
     </div>
-    
-    <?php include('footer.inc.php'); ?>
-    <?php include('../foot.inc.php'); ?>
 
-  </body>
-  </html>
+    <div class="d-flex flex-column align-items-center">       
+      <h2 class="txt-center">La suscriptión al boletín de My Coach Sport está validada.</h2>
+      <a class="m-Top-60" href="../index.php"><div class=" col justify-center button-Connexion"><p class="section-Texte txt-center">Volver a la página de inicio</div></a>
+      </div>
+      <div class="container-fluid p-0">
+        <?php include('footer.inc.php'); ?>
+        <?php include('../foot.inc.php'); ?>
+      </div>
+
+    </body>
+    </html>
