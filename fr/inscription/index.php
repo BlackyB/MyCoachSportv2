f03a073aaf30ffd3b9e332133168d821f3d31167
Bari Jonathan <bari.jonathan@live.fr>
2019-07-09 10:22:00 +0200

    Heroku

--- fr/inscription/index.php
+++ fr/inscription/index.php
@@ -5,13 +5,13 @@
 
   <title>My Coach Sport - Inscription Validée</title>
 </head>
-<body class="col full-H justify-between">
+<body class="full-H d-flex flex-column justify-content-between">
   <?php include('../menu.inc.php'); ?>
 
   <div class="col justify-center align-center w-75 m-auto">       
     <h2 class="txt-center">Votre inscription à la newsletter My Coach Sport est enregistrée.</h2>
     <a class="m-Top-60" href="../index.php"><div class=" col justify-center button-Connexion"><p class="section-Texte txt-center">Retour à l'Accueil</div></a>
-    </div>
+  </div>
     
     <?php include('../footer.inc.php'); ?>
 
